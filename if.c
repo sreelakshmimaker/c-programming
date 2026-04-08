229473feb91823047aeaa10f5a7283966efdf3cf
sreelakshmimaker <sreelakshmierukulla@gmail.com>
2026-04-08 16:13:36 +0530

    Add files via upload

---
+++ if.c
@@ -0,0 +1,14 @@
+
+
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter the value:\n");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("it is an even");
+    }
+    printf("it is an odd");
+}
