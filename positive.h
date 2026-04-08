04360ab20c78cd91ae5efd2f3e857cc248b391eb
sreelakshmimaker <sreelakshmierukulla@gmail.com>
2026-04-08 16:14:40 +0530

    Add files via upload

---
+++ positive.h
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter the number:\n");
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf(" the  given number +ve:\n");
+    }
+    
+        else if (a<0)
+        {
+            printf("the given number is odd");
+        }
+        else if(a==0)
+        {
+            printf("number equal to 0");
+        }
+        }
+
