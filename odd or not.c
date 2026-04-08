a4d1e2da71dc9a92b34532a0c65839c5299a9138
sreelakshmimaker <sreelakshmierukulla@gmail.com>
2026-04-08 16:14:19 +0530

    Add files via upload

---
+++ odd or not.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter the number:\n");
+    scanf("%d",&a);
+    if (a% 2==1)
+    {
+        printf(" the  given number odd:\n");
+    }
+    
+        else 
+        {
+            printf("the given number is not odd");
+        }
+}
+
